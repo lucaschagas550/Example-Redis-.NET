87c65aed6c2f5817915456b0a992af152a0573c4
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-04-15 19:03:43 -0300

    Feat: Redis Manipulando Lista. Feat: Redis Manipulando Conjuntos.

--- Program.cs
+++ Program.cs
@@ -52,23 +52,24 @@ namespace ExampleRedis
             }
 
             //PUB/SUB
-
+            #region 
             var sub1 = db.Multiplexer.GetSubscriber();
             var sub2 = db.Multiplexer.GetSubscriber();
 
-            //first subscribe, until we publish
-            //subscribe to a test message
-            await sub1.SubscribeAsync("test", (channel, message) => {
+            //inscrição
+            await sub1.SubscribeAsync("test", (channel, message) =>
+            {
                 Console.WriteLine("Sub1 Got notification: " + (string)message);
             });
 
-            await sub2.SubscribeAsync("test", (channel, message) => {
+            await sub2.SubscribeAsync("test", (channel, message) =>
+            {
                 Console.WriteLine("Sub2 Got notification: " + (string)message);
             });
 
             //criando um publish
             var publish = db.Multiplexer.GetSubscriber();
-            
+
             //publicando no canal de teste a mensagem
             var count = await publish.PublishAsync("test", "Hello there I am a test message");
             Console.WriteLine($"Number of listeners for test {count}");
@@ -76,11 +77,13 @@ namespace ExampleRedis
 
 
             //padrão bate com a canal, tudo que começa com a A termina com C
-            await sub1.SubscribeAsync(new RedisChannel("a*c", RedisChannel.PatternMode.Pattern), (channel, message) => {
+            await sub1.SubscribeAsync(new RedisChannel("a*c", RedisChannel.PatternMode.Pattern), (channel, message) =>
+            {
                 Console.WriteLine($"Sub1 Got pattern a*c notification: {message}");
             });
-            
-            await sub2.SubscribeAsync(new RedisChannel("a*c", RedisChannel.PatternMode.Pattern), (channel, message) => {
+
+            await sub2.SubscribeAsync(new RedisChannel("a*c", RedisChannel.PatternMode.Pattern), (channel, message) =>
+            {
                 Console.WriteLine($"Sub2 Got pattern a*c notification: {message}");
             });
 
@@ -91,14 +94,112 @@ namespace ExampleRedis
             await publish.PublishAsync("a1234567890c", "Hello there I am a a1234567890c message");
             await publish.PublishAsync("ab", "Hello I am a lost message"); //essa mensagem nunca será enviada
 
-            
+
             //correspondencia automatica com padrões, tudo que começa com as letras primeiras letras iguais
-            await sub1.SubscribeAsync(new RedisChannel("zyx*", RedisChannel.PatternMode.Auto), (channel, message) => {
+            await sub1.SubscribeAsync(new RedisChannel("zyx*", RedisChannel.PatternMode.Auto), (channel, message) =>
+            {
                 Console.WriteLine($"Sub1 Got Literal pattern zyx* notification: {message}");
             });
 
             await publish.PublishAsync("zyxabc", "Hello there I am a zyxabc message");
             await publish.PublishAsync("zyx1234", "Hello there I am a zyxabc message");
+            #endregion
+
+
+            //Manipulando uma lista
+            #region
+            await db.ListLeftPushAsync("site:lista_top3", "palmeiras campeao libertadores 2023");
+            await db.ListLeftPushAsync("site:lista_top3", "palmeiras campeao recopa sul americana 2023");
+            await db.ListLeftPushAsync("site:lista_top3", "palmeiras campeao paulista 2023");
+            await db.ListLeftPushAsync("site:lista_top3", "rivais de sp ficam loucos");
+
+            //0 até -1 que é o ultimo elemento da lista, -2 e -1 seria o penultimo e ultimo elemento
+            var rangeList = await db.ListRangeAsync("site:lista_top3", 0, -1);
+            Console.WriteLine("\n");
+            foreach (var item in rangeList)
+            {
+                Console.WriteLine($"Lista: {item}");
+            }
+
+            var oldNews = await db.ListRightPopAsync("site:lista_top3");
+            Console.WriteLine($"\nRemovendo noticia mais antiga: {oldNews}");
+
+            var newList = await db.ListRangeAsync("site:lista_top3", 0, 2);
+            Console.WriteLine("\n");
+            foreach (var item in newList)
+            {
+                Console.WriteLine($"Lista Top 3 noticias: {item}");
+            }
+
+            var deleteKeyList = await db.KeyDeleteAsync("site:lista_top3");
+            Console.WriteLine($"\nLista deletada? {deleteKeyList}");
+            #endregion
+
+            //Manipulando Conjuntos
+            #region
+            await db.SetAddAsync("conjunto:amigos_lucas", "patrick");
+            await db.SetAddAsync("conjunto:amigos_lucas", "felipe");
+            await db.SetAddAsync("conjunto:amigos_lucas", "lucas");
+            await db.SetAddAsync("conjunto:amigos_lucas", "daniel");
+            await db.SetAddAsync("conjunto:amigos_lucas", "vinicius");
+
+            await db.SetAddAsync("conjunto:amigos_exercito_brasileiro", "lucas");
+            await db.SetAddAsync("conjunto:amigos_exercito_brasileiro", "joao");
+            await db.SetAddAsync("conjunto:amigos_exercito_brasileiro", "ricardo");
+            await db.SetAddAsync("conjunto:amigos_exercito_brasileiro", "daniel");
+            await db.SetAddAsync("conjunto:amigos_exercito_brasileiro", "bruno");
+            await db.SetAddAsync("conjunto:amigos_exercito_brasileiro", "caio");
+
+            Console.WriteLine("\n");
+            var totalMembersLucas = await db.SetMembersAsync("conjunto:amigos_lucas");
+            var totalMembersEB = await db.SetMembersAsync("conjunto:amigos_exercito_brasileiro");
+
+            Console.WriteLine("\n");
+            foreach (var item in totalMembersLucas)
+            {
+                Console.WriteLine($"Amigo do Lucas: {item}");
+            }
+
+            Console.WriteLine("\n");
+            foreach (var item in totalMembersEB)
+            {
+                Console.WriteLine($"Amigos EB: {item}");
+            }
+
+            Console.WriteLine("\n");
+            var IsFriendLucas = await db.SetContainsAsync("conjunto:amigos_lucas", "patrick");
+            Console.WriteLine($"É amigo do lucas? {IsFriendLucas}");
+
+            var IsFriendEB = await db.SetContainsAsync("conjunto:amigos_exercito_brasileiro", "Jhonathan");
+            Console.WriteLine($"É amigo do EB? {IsFriendEB}");
+
+            Console.WriteLine("\n");
+            var removeFriendEB = await db.SetRemoveAsync("conjunto:amigos_exercito_brasileiro", "caio");
+            Console.WriteLine($"Amizade removida? {removeFriendEB}");
+
+            //Amigos em comum nos dois conjuntos
+            Console.WriteLine("\n");
+            var mutualFriends = await db.SetCombineAsync(SetOperation.Intersect, "conjunto:amigos_lucas", "conjunto:amigos_exercito_brasileiro");
+            foreach(var item in mutualFriends)
+            {
+                Console.WriteLine($"Amigos em comum: {item}");
+            }
+
+            //Recupera os diferentes amigos do primeiro conjunto para o segundo, resultando em patrick, vinicius e felipe
+            Console.WriteLine("\n");
+            var differentFriends = await db.SetCombineAsync(SetOperation.Difference, "conjunto:amigos_lucas", "conjunto:amigos_exercito_brasileiro");
+            foreach (var item in differentFriends)
+            {
+                Console.WriteLine($"Pessoas não conhecidas: {item}");
+            }
+
+
+            var deleteKeySetLucas = await db.KeyDeleteAsync("conjunto:amigos_lucas");
+            Console.WriteLine($"\nConjunto de amigos do lucas deletado? {deleteKeySetLucas}"); 
+            
+            var deleteKeySetEb = await db.KeyDeleteAsync("conjunto:amigos_exercito_brasileiro");
+            Console.WriteLine($"\nConjunto de amigos do EB deletado? {deleteKeySetEb}");
+            #endregion
 
 
             Console.ReadKey();
